5fab9fc8e20a9e360a3f7b6a41726651808f3971
Rolle <roland.schmitt@web.de>
2015-03-03 21:00:33 +0100

    navigation

--- platforms/android/ant-build/classes.dex.d
+++ platforms/android/ant-build/classes.dex.d
@@ -1,9 +1,9 @@
-C:\Projects\phonegap\GWW\platforms\android\ant-build\classes.dex : \
-C:\Projects\phonegap\GWW\platforms\android\ant-build\classes\de\bergwerkfell\BuildConfig.class \
-C:\Projects\phonegap\GWW\platforms\android\ant-build\classes\de\bergwerkfell\CordovaApp.class \
-C:\Projects\phonegap\GWW\platforms\android\ant-build\classes\de\bergwerkfell\R$attr.class \
-C:\Projects\phonegap\GWW\platforms\android\ant-build\classes\de\bergwerkfell\R$drawable.class \
-C:\Projects\phonegap\GWW\platforms\android\ant-build\classes\de\bergwerkfell\R$string.class \
-C:\Projects\phonegap\GWW\platforms\android\ant-build\classes\de\bergwerkfell\R$xml.class \
-C:\Projects\phonegap\GWW\platforms\android\ant-build\classes\de\bergwerkfell\R.class \
-C:\Projects\phonegap\GWW\platforms\android\ant-build\dexedLibs\classes-55fd8e48b054c24424a6e9fb48d17021.jar \
+c:\Users\roland\projects\GWW-App\platforms\android\ant-build\classes.dex : \
+c:\Users\roland\projects\GWW-App\platforms\android\ant-build\classes\de\bergwerkfell\BuildConfig.class \
+c:\Users\roland\projects\GWW-App\platforms\android\ant-build\classes\de\bergwerkfell\CordovaApp.class \
+c:\Users\roland\projects\GWW-App\platforms\android\ant-build\classes\de\bergwerkfell\R$attr.class \
+c:\Users\roland\projects\GWW-App\platforms\android\ant-build\classes\de\bergwerkfell\R$drawable.class \
+c:\Users\roland\projects\GWW-App\platforms\android\ant-build\classes\de\bergwerkfell\R$string.class \
+c:\Users\roland\projects\GWW-App\platforms\android\ant-build\classes\de\bergwerkfell\R$xml.class \
+c:\Users\roland\projects\GWW-App\platforms\android\ant-build\classes\de\bergwerkfell\R.class \
+c:\Users\roland\projects\GWW-App\platforms\android\ant-build\dexedLibs\classes-ced369ab7ff7eb5b3870680dfbcb2ec8.jar \
